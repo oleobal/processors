89d99e169beaa8960d110c7a7086f6f2197801de
Olivier Leobal <olivier.contrib@leobal.eu>
2018-08-17 21:12:36 +0200

    Added actual operation STORE

--- v16alpha.md
+++ v16alpha.md
@@ -1,29 +1,60 @@
 V16α processor (V16alpha)
 =========================
 
-One 16-bits register `register`. General purpose, used for arithmetic. 
-
-One 16-bits register `io`, supposed to be connected to a bus for I/O.
-
-One 8-bits register `status`, set by the processor during operation.
-
-One unlimited-size `program` (list of string instructions)
-
-One 16-bits register `programCounter`, addressing string instructions.
+The V16alpha is an amazing processor that manages to work DIRECTLY from
+assembly code! This does put some restrictions on the user, but we are
+confident our clients are competent enough.
 
 
+### Facilities
+
+The following registers are provided :
+
+ - One 16-bits register `RINT`, general purpose, used for arithmetic
+ - One 16-bits register `RINO`, supposed to be connected to a bus
+ 
+In addition, other registers are used for operation of mostly automatic
+components :
+ 
+ - One 8-bits  register `RERR`, set by the processor during operation
+ - One 16-bits register `RCNT`, pointing to the current instructions
+ - One 4-bits register `RSTA`, pointing to the current stack level
+
+Because the V16alpha program memory contains assembly and not machine
+code, it is not possible to retrieve values from it.
+
+The stack addressed by `RSTA` is a memory unit of 256 bits, which is
+split in thirty-two 8-bits values.
+
+### Operations
+
+Assembly for the V16alpha is a list of commands of the form :
+
+`<OP> <target or value> [target or value]`
+
+Operations :
+
+|  OP   | operand 1 | operand 2 | Description            |
+|-------|-----------|-----------|------------------------|
+| STORE | value/reg | reg       | Stores oper1 in oper2  |
+|       |           |           |                        |
 
 ### Status codes
 
-Available by checking `status`, an 8-bit register. Codes :
+Available by checking `RERR`, an 8-bit register. Codes :
 
-| Code | Hex | Status             |
-|------|-----|--------------------|
-| 0    | 0   | Processor ready    |
-| 1    | 1   | No program         |
-| 2    | 2   | Finished execution |
-|      |     |                    |
-| 255  | FF  | Other              |
+| Code | Hex | Status                   |
+|------|-----|--------------------------|
+| 0    | 0   | Processor ready          |
+| 1    | 1   | No program               |
+| 2    | 2   | Executing                |
+| 3    | 3   | Finished execution       |
+|      |     |                          |
+| 10   | A   | Invalid operation        |
+| 11   | B   | Invalid operand          |
+| 12   | C   | Wrong number of operands |
+|      |     |                          |
+| 255  | FF  | Other                    |
 
 
 
--- v16alpha.py
+++ v16alpha.py
@@ -5,23 +5,32 @@ class V16alpha(Processor)  :
 	A simple processor
 	Features :
 	 - an unlimited program memory (list of instructions)
-	 - a programCounter pointing at the current instruction
-	 - one 16 bits register that overflows by resetting to 0
-	 - and underflows by going to max value
+	 - a 16-bits programCounter register pointing at the current instruction
+	 - one general purpose 16 bits register
 	 - a stack with automatic operation (eg no manual counter)
 	 - an IO 16-bits register presumably connected to a bus
 	   (behaves like the internal register, but can be set by outside forces)
 	 - an 8-bit register for status/error codes
 	"""
 	
+	# cost of ops in cycles
+	# of course this is nonsensical, especially if I want to implement
+	# a pipeline some day
+	# but really this whole thing is nonsensical
+	operations={
+	"STORE":2,
+	"ADD":3
+	}
+	
 	def __init__(self):
 		self.cycleCount=0
 		self.program = []
 		self.programCounter= Register(16, name="COUNTER")
 		self.register = Register(16, name="INTERNAL")
-		self.stack = []
+		self.stack = [] #TODO stack structure
+		self.stackPointer = Register(4, name="STACK")
 		self.io = Register(16, name="IO")
-		self.status = Register(8, name="STATUS")
+		self.err = Register(8, name="ERROR")
 		# TODO catch exceptions and put them here instead
 		
 		self.instructionQueue=deque()
@@ -33,6 +42,53 @@ class V16alpha(Processor)  :
 		# so basically it's some kind of potential pipeline
 	
 
+	def parseInstruction(self, instruction):
+		"""
+		converts a string into a tuple
+		(op, target[, target])
+		"""
+		tab = instruction.upper().split()
+		
+		t1 = tab[1]
+		try:
+			t1 = int(t1)
+		except ValueError:
+			if t1 == "RINT":
+				t1 = self.register
+			elif t1 == "RERR":
+				t1 = self.err
+			elif t1 == "RINO":
+				t1 = self.io
+			elif t1 == "RCNT":
+				t1 = self.programCounter
+			elif t1 == "RSTA":
+				t1 = self.stackPointer
+			else:
+				self.err = 11
+		
+		if len(tab) == 2 :
+			return (tab[0], t1)
+		
+		t2 = tab[2]
+		try:
+			t2 = int(t2)
+		except ValueError:
+			if t2 == "RINT":
+				t2 = self.register
+			elif t2 == "RERR":
+				t2 = self.err
+			elif t2 == "RINO":
+				t2 = self.io
+			elif t2 == "RCNT":
+				t2 = self.programCounter
+			elif t2 == "RSTA":
+				t2 = self.stackPointer
+			else:
+				self.err.value = 11
+		
+		return (tab[0],t1,t2)
+			
+	
 	
 	def loadProgram(self,code):
 		"""
@@ -41,25 +97,73 @@ class V16alpha(Processor)  :
 		and resets programCounter to 0
 		"""
 		self.program = code
-		self.programCounter=0
-		self.status = 0
+		self.programCounter.value=0
+		self.err.value = 0
 	
 	
 	def cycle(self):
 		"""
 		One processor cycle.
 		"""
+		self.cycleCount += 1
+		self.err.value = 2
+		
+		
 		if len(self.program) == 0:
-			self.status.value = 1
+			self.err.value = 1
 			return
-		if self.programCounter == len(self.program) :
-			self.status.value = 2
+		if self.programCounter.value == len(self.program) :
+			self.err.value = 3
 			return
 		
+		try :
+			instruction = self.parseInstruction(self.program[self.programCounter.value])
+			
+			op = instruction[0]
+			t1 = instruction[1]
+			if len(instruction) > 2 :
+				t2 = instruction[2]
+		except Exception as e:
+			return
+		
+		if op not in V16alpha.operations:
+			self.err.value = 10
+			return
+		
+
+		
+		if op == "STORE":
+			if len(instruction) != 3:
+				self.err.value = 12
+				return
+			if type(t1) is int :
+				a = t1
+			elif type(t1) is Register:
+				a = t1.value
+			
+			
+			try:
+				t2.value = a
+			except Exception:
+				self.err.value = 11
+				return
+
+
+
+
+
+		
+		self.programCounter.value+=1
 
 
 if __name__ == '__main__' :
 	p = V16alpha()
-	print(p.status)
+	print(p.err)
 	p.cycle()
-	print(p.status)
+	print(p.err)
+	print(p.register)
+	p.loadProgram(["STORE 10 RINT"])
+	while (p.err.value < 3):
+		p.cycle()
+		print(p.register)
+		print(p.err)
