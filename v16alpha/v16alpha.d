c4c71889e1409781b4b13c28472003adb9498664
oleobal <olivier.contrib@leobal.eu>
2018-11-28 16:42:04 +0100

    Start adding v16a instructions
    This time, unified mechanism.

--- v16alpha/v16alpha.d
+++ v16alpha/v16alpha.d
@@ -8,6 +8,108 @@ import std.math;
 
 import processor;
 
+struct instr
+{
+	string name;
+	ubyte code;
+	ushort time;
+}
+
+static struct instructions
+{
+	immutable instr[] list = [
+		instr("STORE"	, 0xA0, 2),
+		instr("DLPR"	, 0xA1, 3),
+		instr("DSPR"	, 0xA2, 3),
+		instr("DLST"	, 0xA3, 3),
+		instr("DSST"	, 0xA4, 3),
+		instr("PUSH"	, 0xA5, 2),
+		instr("POP" 	, 0xA6, 2),
+		instr("LABEL"	, 0xA7, 1),
+		instr("JUMP"	, 0xA8, 1),
+		
+		
+		instr("ADD" 	, 0xB0, 2),
+		instr("REM" 	, 0xB1, 2),
+		instr("MUL" 	, 0xB2, 3),
+		instr("DIV" 	, 0xB3, 3),
+		instr("MODU"	, 0xB4, 2),
+		instr("AND" 	, 0xB5, 1),
+		instr("OR"  	, 0xB6, 1),
+		instr("XOR" 	, 0xB7, 1),
+		
+		instr("LSHIFT"	, 0xBA, 1),
+		instr("RSHIFT"	, 0xBB, 1),
+		
+		instr("IFEQ"	, 0xC0, 2),
+		instr("IFLT"	, 0xC1, 2),
+		instr("IFLE"	, 0xC2, 2),
+		instr("IFGT"	, 0xC3, 2),
+		instr("IFGE"	, 0xC4, 2),
+		instr("IFNE"	, 0xC5, 2),
+		
+		instr("END" 	, 0xCF, 1),
+		
+		
+		instr("RINT" 	, 0xD0, 0),
+		instr("RERR" 	, 0xD1, 0),
+		instr("RIO" 	, 0xD2, 0),
+		instr("PC"  	, 0xD3, 0), // program counter
+		instr("SP"  	, 0xD4, 0), // stack pointer
+		instr("RIOA" 	, 0xD5, 0),
+		instr("RIOB" 	, 0xD6, 0),
+		instr("RIOAL" 	, 0xD7, 0),
+		instr("RIOAR" 	, 0xD8, 0),
+		instr("RINTL" 	, 0xD9, 0),
+		instr("RINTR" 	, 0xDA, 0),
+		
+		instr("LADD"	, 0xE0, 2),
+		instr("LREM"	, 0xE1, 2),
+		instr("LMUL"	, 0xE2, 3),
+		instr("LDIV"	, 0xE3, 3),
+		instr("LMODU"	, 0xE4, 3),
+		instr("LAND"	, 0xE5, 1),
+		instr("LOR" 	, 0xE6, 1),
+		instr("LXOR"	, 0xE7, 1),
+		instr("LSTORE"	, 0xE8, 1),
+	];
+
+	
+	
+	immutable instr[ubyte] byCode;
+	immutable instr[string] byName;
+
+	/**
+	 * TODO In a perfect world these associative arrays over would be statically
+	 * filled with Dlang's CTFE. 
+	 * right now these functions do their own setup which is stupid
+	 */
+	 
+	instr get(string name) const
+	{
+		if (byName.length == 0)
+		{
+			foreach (i ; list)
+				byName[i.name] = i;
+		}
+		return byName[name];
+
+	}
+	instr get(ubyte  code) const
+	{
+		if (byCode.length == 0)
+		{
+			foreach (i ; list)
+				byCode[i.code] = i;
+		}
+		return byCode[code];
+	}
+	
+}
+
+
+
+
 class V16alpha : Processor
 {
 	ulong cycleCount;
@@ -90,6 +192,22 @@ class V16alpha : Processor
 		
 		currentInstruction = [0xFF, 0xFF, 0xFF];
 		currentInstructionState = 0;
+	}
+	
+	
+	/**
+	 * One processor cycle
+	 * relies on state (currentInstruction, currentInstructionState..)
+	 *
+	 * triggered by turning the (I Clock) pin to 1 (it resets it to 0)
+	 */
+	void cycle()
+	{
+		clock.setPin(0, false);
+		cycleCount++;
+		
+		
+		
 	}
 }
 
